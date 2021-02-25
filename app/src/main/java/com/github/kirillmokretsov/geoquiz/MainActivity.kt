504bdea2a4974bbc334c9bf76f6bb2aa2f86d6dc
Kirill-Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-02-25 12:47:12 +0300

    Init buttons in MainActivity.kt

--- app/src/main/java/com/github/kirillmokretsov/geoquiz/MainActivity.kt
+++ app/src/main/java/com/github/kirillmokretsov/geoquiz/MainActivity.kt
@@ -2,10 +2,18 @@ package com.github.kirillmokretsov.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonTrue: Button
+    private lateinit var buttonFalse: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonTrue = findViewById(R.id.button_true)
+        buttonFalse = findViewById(R.id.button_false)
     }
 }
