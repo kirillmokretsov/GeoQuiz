393faf9480f9c2588d0be2ff02f81c1f58e6176e
Kirill-Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-02-27 12:10:07 +0300

    Create CheatActivity.kt and layout for it

---
+++ app/src/main/java/io/github/kirillmokretsov/geoquiz/CheatActivity.kt
@@ -0,0 +1,11 @@
+package io.github.kirillmokretsov.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+    }
+}
