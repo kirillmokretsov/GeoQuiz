9f09ffc2582df7e08c0636e53dbda9a1d72ba4de
Kirill-Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-02-27 12:36:49 +0300

    Reveal answer if buttonShowAnswer pressed

--- app/src/main/java/io/github/kirillmokretsov/geoquiz/CheatActivity.kt
+++ app/src/main/java/io/github/kirillmokretsov/geoquiz/CheatActivity.kt
@@ -3,12 +3,18 @@ package io.github.kirillmokretsov.geoquiz
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.github.kirillmokretsov.geoquiz.R
 
 private const val EXTRA_ANSWER_IS_TRUE = "io.github.kirillmokretsov.geoquiz.answer_is_true"
 
 class CheatActivity : AppCompatActivity() {
+
+    private var answerIsTrue = false
+    private lateinit var textViewAnswer: TextView
+    private lateinit var buttonShowAnswer: Button
 
     companion object {
         fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent =
@@ -20,5 +26,18 @@ class CheatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheat)
+
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+
+        textViewAnswer = findViewById(R.id.text_view_answer)
+        buttonShowAnswer = findViewById(R.id.button_show_answer)
+
+        buttonShowAnswer.setOnClickListener {
+            val answerText = when {
+                answerIsTrue -> R.string.button_true
+                else -> R.string.button_false
+            }
+            textViewAnswer.setText(answerText)
+        }
     }
 }
